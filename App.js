b75ed6e465edd1c120bece20cd7966f658bdc11f
adimehrotra1 <109669425+adimehrotra1@users.noreply.github.com>
2022-07-27 12:32:26 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Banner from './Banner';
+import Card from './Card';
+import './Card.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Banner />
+      
+        
+        
+    
+  
+      
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ Banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='Banner'>Sextant</div>
+  )
+}
+
+export default Banner
